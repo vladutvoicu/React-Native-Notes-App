0ffff505c60170485fd3907afceefaff620789f7
Vladut Voicu <voicu.vladutdaniel@yahoo.com>
2022-06-14 22:34:01 +0300

    added 'remember me' feature

--- App/index.js
+++ App/index.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navigation from "./config/navigation";
+import { authentication } from "./config/firebase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 console.disableYellowBox = true;
 
-export default () => <Navigation />;
+export default () => {
+  useEffect(() => {
+    const _retrieveData = async () => {
+      try {
+        const value = await AsyncStorage.getItem("keepLoggedIn");
+        if (value == "false") {
+          authentication.signOut();
+        } else {
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    _retrieveData();
+  }, []);
+  return <Navigation />;
+};
